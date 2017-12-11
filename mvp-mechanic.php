c2f6f607d4ed91be314201cb73a46bc128687715
midwinterboxes <office@binarygeometry.co.uk>
2017-12-11 18:15:43 +0000

    removed boilerplate code

--- mvp-mechanic.php
+++ mvp-mechanic.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: MVP Mechanic
+Plugin Name: Lets Tzu This
 Plugin URI: http://nowbotica.com/lets-tzu-this/
 Description: A rust proof diy starter kit for miro enterprises
 Author: Andrew MacKay
@@ -12,10 +12,10 @@ define( 'MVPMSYSTEM', plugin_dir_path( __FILE__ ) );
 define( 'MVPMSYSTEM_URL', plugin_dir_url( __FILE__ ) );
 
 # Includes system wide helpers
-include( MVPMSYSTEM . '/parts/System.php');
+// include( MVPMSYSTEM . '/parts/System.php');
 
 # Includes the user mangement module
-include( MVPMSYSTEM . '/parts/User.php');
+// include( MVPMSYSTEM . '/parts/User.php');
 
 # Includes the Profile module
 // include( MVPMSYSTEM . '/parts/Profile.php');
@@ -57,8 +57,8 @@ add_shortcode('mvpmApp', 'mvpmApp');
 //    preg_match('/'.$pattern.'/s', $posts[0]->post_content, $matches);
 //    if (is_array($matches) && $matches[2] == 'mvmpApp') {
         //shortcode is being used
-        add_action('wp_enqueue_scripts','mvmpApp_css');
-        add_action('wp_enqueue_scripts','mvmpApp_scripts');
+        // add_action('wp_enqueue_scripts','mvmpApp_css');
+        // add_action('wp_enqueue_scripts','mvmpApp_scripts');
 //    }
 //}
 //add_action('template_redirect','mvmpApp_head');
@@ -167,15 +167,15 @@ if ( ! function_exists( 'mvmpApp_scripts' ) ) {
 }
 
 
-function remove_dashboard_meta() {
-        remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
-        remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
-        remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
-        remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
-        remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
-        remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
-        remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
-        remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
-        remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');//since 3.8
-}
-add_action( 'admin_init', 'remove_dashboard_meta' );
+// function remove_dashboard_meta() {
+//         remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+//         remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+//         remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+//         remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
+//         remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+//         remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+//         remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+//         remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+//         remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');//since 3.8
+// }
+// add_action( 'admin_init', 'remove_dashboard_meta' );
